479377368eb21938ecfb3a90099398063726506c
Clayton William <clayton.calvin@gmail.com>
2017-07-30 01:10:43 -0400

    restore perl file

--- purgeUnneededExtFromCWD.pl
+++ purgeUnneededExtFromCWD.pl
@@ -2,23 +2,17 @@
 
 use 5.010;
 use Cwd;
-use File::Find::Rule;
-use File::Basename qw(basename);
-use Data::Dump;
 
 my $currentDir = getcwd;
-my @fullPath = File::Find::Rule->file->name('*.pyc')->in($path);
-
-print Dumper @fullPath;
 
 
-# my @unneededFiles = glob("*~ *# *pyc");
-# my $unneededFiles = @unneededFiles;
-#
-# if ($unneededFiles > 0) {
-#     foreach my $file (@unneededFiles) {
-#         unlink $file || warn "Could not unlink $file: $!";
-#     }
-# } else {
-#     say "INFO: No extensions removed."
-# }
+my @unneededFiles = glob("*~ *# *pyc");
+my $unneededFiles = @unneededFiles;
+
+if ($unneededFiles > 0) {
+    foreach my $file (@unneededFiles) {
+        unlink $file || warn "Could not unlink $file: $!";
+    }
+} else {
+    say "INFO: No extensions removed."
+}
